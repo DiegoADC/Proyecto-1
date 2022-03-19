ec9c195410aa7ba7cb0d85bb15f0698a89d27eb1
diego_cruz <diego_cruz@outlook.com>
2022-03-19 05:46:24 -0600

    quarter commit

--- Scrips/app.js
+++ Scrips/app.js
@@ -1,11 +1,13 @@
+// Constantes del Navbar
 const nav = document.querySelector('#hamburger button');
 const icon = document.querySelector('#hamburger'),
     menu = document.querySelector('#menu');
 
+// Menu Hamburguesa
 nav.addEventListener('click', e =>{
     nav.classList.toggle('open');
 });
-
+// Menu lateral
 icon.addEventListener('click', (e) => {
     menu.classList.toggle('b-content-active');
 })
